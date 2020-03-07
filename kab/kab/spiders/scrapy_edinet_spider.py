8514251aa1786738baae156c522a602842bff9fc
tinmanCtr <ryo.prfm27@gmail.com>
2020-03-08 00:59:28 +0900

    spider実装 yield itemまで

--- kab/kab/spiders/scrapy_edinet_spider.py
+++ kab/kab/spiders/scrapy_edinet_spider.py
@@ -6,18 +6,21 @@ import re
 import zipfile
 import os
 import time
+from datetime import datetime, date, timedelta
 import shutil
 from pathlib import Path
 from kab.items import KabItem
 import glob
 
-direc = Path(__file__).parent  # 現在のディレクトリ
-dl_path = direc / 'DL/'
-out_path = direc / 'out/'
+current_dir = Path(__file__).parent
+dl_path = current_dir / 'DL/'
+out_path = current_dir / 'out/'
 
-
-dl_path = str(dl_path) + '/'
-out_path = str(out_path) + '/'
+DL_FOLDER = str(dl_path) + '/'
+OUT_FOLDER = str(out_path) + '/'
+XBRL_REGEXP = re.compile(r'XBRL/PublicDoc/jpcrp[0-9]{6}-.{3}-[0-9]{3}_E[0-9]{5}-[0-9]{3}_[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}_[0-9]{4}-[0-9]{2}-[0-9]{2}.xbrl')
+START_DATE = date(2019,1,1)
+END_DATE = date(2019,12,1)
 
 
 def download_file (fname, path='./'):
@@ -40,10 +43,9 @@ def download_file (fname, path='./'):
 def unzip_xbrl_returns_file_name (fname, inputpath, outputpath):
   fp = inputpath + fname + '.zip'
   with zipfile.ZipFile(fp) as zip_e:
-    file_name_pattern = re.compile(r'XBRL/PublicDoc/jpcrp[0-9]{6}-.{3}-[0-9]{3}_E[0-9]{5}-[0-9]{3}_[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}_[0-9]{4}-[0-9]{2}-[0-9]{2}.xbrl')
     n_l = zip_e.infolist()
     for i in n_l:
-      match_obj = file_name_pattern.match(i.filename)
+      match_obj = XBRL_REGEXP.match(i.filename)
       if match_obj:
         print('file found!')
         zip_e.extract(i, outputpath)
@@ -73,10 +75,14 @@ def extract_from_xbrl (filepath):
   # return xbrl_infos[]
   pass
 
+
 class ScrapyEdinetSpiderSpider(scrapy.Spider):
   name = 'scrapy_edinet_spider'
   allowed_domains = ['disclosure.edinet-fsa.go.jp']
-  start_urls = ['https://disclosure.edinet-fsa.go.jp/api/v1/documents.json?date=2019-11-29&type=2']
+  start_urls = [
+    "https://disclosure.edinet-fsa.go.jp/api/v1/documents.json?date={}&type=2"
+    .format(str(START_DATE + timedelta(i))) for i in range((END_DATE - START_DATE).days - 1)
+  ]
 
   def parse(self, response):
     """
@@ -88,36 +94,62 @@ class ScrapyEdinetSpiderSpider(scrapy.Spider):
     print('レスポンス受け取り成功、処理開始')
     print(len(res_json))
 
-    # ダウンロードするzipを決めるリスト
-    docIDs = []
-
     # xbrlを参照しなくても得られる情報
     meta_infos = []
 
     # xbrlを解析しないとわからない情報
     xbrl_infos = []
-    
 
     for doc in res_json:
       # 雑多なデータを無視して有報と四半期報告書のみをダウンロード対象にする。
       if doc['ordinanceCode'] == '010':
         if doc['formCode'] == '043000' or doc['formCode'] == '030000':
           if doc['secCode']:
-            
-            docIDs.append(doc['docID'])
 
-            meta_infos.append((doc['secCode'], doc['filerName'], doc['docDescription'], doc['periodStart'], doc['periodEnd']))
-          
+            meta_infos.append({
+              'docID': doc['docID'],
+              'secCode': doc['secCode'],
+              'filerName': doc['filerName'],
+              'docDescription': doc['docDescription'],
+              'periodStart': doc['periodStart'],
+              'periodEnd': doc['periodEnd'],
+            })
 
 
-    
-    from_api_to_xbrl(docIDs, dl_path, out_path)
+    for i in range(len(meta_infos)):
+      file_url = 'https://disclosure.edinet-fsa.go.jp/api/v1/documents/' + meta_infos[i]['docID'] + '?type=1'
+      yield scrapy.Request(file_url, callback=self.parse_file,
+                           meta=meta_infos[i])
 
-    for i in range(len(docIDs)):
-      yield KabItem(
-        secCode = meta_infos[i][0],
-        filerName = meta_infos[i][1],
-        docDescription = meta_infos[i][2],
-        periodStart = meta_infos[i][3],
-        periodEnd = meta_infos[i][4],
-      )
+  def parse_file(self, response):
+    chunks = [response.body[i: i+1024] for i in range(0, len(response.body), 1024)]
+    zip_filepath = DL_FOLDER + response.meta['docID'] + '.zip'
+
+    # zipファイル保存
+    with open(zip_filepath, 'wb') as f:
+      for chunk in chunks:
+        if chunk:
+          f.write(chunk)
+          f.flush()
+    print('file: ' + response.meta['docID'] + " has been downloaded!")
+
+    # zip解凍、XBRL抽出、余分なファイル削除
+    with zipfile.ZipFile(zip_filepath) as zip_e:
+      n_l = zip_e.infolist()
+      for i in n_l:
+        match_obj = XBRL_REGEXP.match(i.filename)
+        if match_obj:
+          print('file found!')
+          zip_e.extract(i, OUT_FOLDER)
+          print('unzipped!')
+          shutil.move(OUT_FOLDER + match_obj.group(), OUT_FOLDER)
+          shutil.rmtree(OUT_FOLDER + 'XBRL/')
+          os.remove(zip_filepath)
+
+    yield KabItem(
+      secCode = response.meta['secCode'],
+      filerName = response.meta['filerName'],
+      docDescription = response.meta['docDescription'],
+      periodStart = response.meta['periodStart'],
+      periodEnd = response.meta['periodEnd'],
+    )
